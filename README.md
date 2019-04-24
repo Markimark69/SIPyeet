c7884e14c74d8ea902389b43a092701c59f8dce8
Niklas <Alphakilo@users.noreply.github.com>
2019-04-24 20:31:18 +0200

    added -c for yeet count; expanded docs somwehat

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # SIPyeet
+[*Discard SIP messages at high velocity.*](https://www.urbandictionary.com/define.php?term=Yeet)
 
 Pst! Hey! You! Yes, you!\
 Wanna yeet somewhat RFC compliant SIP messages at unsuspecting SIP UAs?
 
 Then you've come to the right place!
 
-Remember kids, research only!
+Remember kids: research only!
+
+## Why though? 🤔
+
+Short answer: ¯\\_(ツ)\_/¯
+
+Long answer: While troubleshooting some SIP troubles I found the lack of tools that craft singular SIP messages disturbing.\
+ If you need to handle responses (SIP dialogs) and sessions, this isn't the right tool. Check out SIPp and prepare your editor for huge XML docs.
 
 ## Installation
 ### Requirements
@@ -14,5 +22,11 @@ The core of this gadget is [scapy](https://scapy.net/). Go get that first.
 
 ## Usage
 
-`-src` sets the source address, Scapy will resolve hostnames before putting the Packet on the wire. SIPyeet will not so hostnames will remain as such in SIP FROM-fields.\
-`-dst` sets the destination address and TO-fields.
+`-src`, `-dst` sets the source / destination address respectively. Scapy will resolve hostnames before putting the Packet on the wire. SIPyeet will not, so hostnames will remain as such in SIP FROM- / TO-fields.\
+`-tt` sets the transport mode. If `-tt` is not supplied, SIPyeet defaults to UDP.
+
+## Methods
+
+### OPTIONS
+Currently the only implemented SIP Method is OPTIONS.\
+Usually used to do weird stuff to physical SIP UAs (i.e. deskphones), like asking them to reboot and reprovision, but it also finds itself being used as a keep-alive method.<sup>Citation needed</sup>
--- sipyeet.py
+++ sipyeet.py
@@ -11,13 +11,15 @@ parser.add_argument("-dp", type=int, default=5060, help="The destination port")
 parser.add_argument("-tt", action="store_true", help="Transport TCP")
 parser.add_argument("-dst", default="127.0.0.2", help="The destination IP")
 parser.add_argument("-src", default="127.0.0.1", help="The source IP")
+parser.add_argument("-c", type=int, default=1, help="Packet count per destination")
+
 parser.add_argument("-fuser", type=str, default="yeet", help="SIP FROM User")
 parser.add_argument("-tuser", type=str, default="yoink", help="SIP TO User")
 
 parser.add_argument("--no-rfc3261-branches", action="store_false", help="Don't use the 'z9hG4bK'-prefix in Via/Branch. See https://tools.ietf.org/html/rfc3261#section-8.1.1.7")
 
 # Flow related arguments
-parser.add_argument("--dry-run", action="store_true", help="Don't actually send pakets.")
+parser.add_argument("--dry-run", action="store_true", help="Don't actually send packets.")
 parser.add_argument("-v", action="store_true", help="Be more verbose about the yeetage.")
 
 parser.parse_args()
@@ -30,35 +32,33 @@ sourceIp = args.src
 
 sipTag = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
 sipBranch = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+sipCallId = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
+
+ip=IP(src=sourceIp, dst=destinationIp)
 
 if args.no_rfc3261_branches:
     sipBranch = ("z9hG4bK{0}".format(sipBranch))
-
 
 if args.tt:
     transport = "tcp"
 else:
     transport = "udp"
 
-ip=IP(src=sourceIp, dst=destinationIp)
-
 myPayload=(
     'OPTIONS sip:{0}:{1};transport={6} SIP/2.0\r\n'
     'Via: SIP/2.0/{7} {2}:{3};branch={8}\r\n'
     'From: \"{4}\"<sip:{4}@{2}:{3}>;tag={9}\r\n'
     'To: \"{5}\" <sip:{5}@{0}:{1}>\r\n'
-    'Call-ID: 9abcd\r\n'
-    'CSeq: 1 OPTIONS\r\n'
-    'Max-Forwards: 0\r\n'
+    'Call-ID: {10}\r\n'
     'Content-Length: 0\r\n\r\n').format(destinationIp, destinationPort, sourceIp, sourcePort, 
-        args.fuser, args.tuser, transport, transport.upper(), sipBranch, sipTag)
+        args.fuser, args.tuser, transport, transport.upper(), sipBranch, sipTag, sipCallId)
 
 if args.v:
     print("Payload to be yeeted:")
     print(myPayload)
 
 if not args.dry_run:
-    if args.tt:
+    if transport=="tcp":
         # TCP SYN
         TCP_SYN=TCP(sport=sourcePort, dport=destinationPort, flags="S", seq=100)
         TCP_SYNACK=sr1(ip/TCP_SYN)
@@ -69,9 +69,9 @@ if not args.dry_run:
         send(ip/TCP_ACK)
 
         TCP_PUSH=TCP(sport=sourcePort, dport=destinationPort, flags="PA", seq=101, ack=myAck)
-        send(ip/TCP_PUSH/myPayload)
+        send(ip/TCP_PUSH/myPayload,count=args.c)
     else:
         UDP_PUSH=UDP(sport=sourcePort, dport=destinationPort)
-        send(ip/UDP_PUSH/myPayload)
+        send(ip/UDP_PUSH/myPayload,count=args.c)
 else:
-    print("No SIP UA{C,S} where harmed 🙃")
+    print("Dry run. No SIP UAs where harmed in the process of generating this message 🙃")
